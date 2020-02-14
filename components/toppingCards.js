e693d3a7bf39106fcd59150235ffe2de7e5b3df3
Darshan Tripathi <darshantripathi909@gmail.com>
2020-02-14 12:16:05 +0530

    Add2

---
+++ components/toppingCards.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { View, 
+        StyleSheet, 
+        Text, 
+        Image, 
+        TouchableOpacity } from 'react-native'
+import GLOBAL from '../constants/global'
+export default function ToppingCards(props) {
+    return(
+        <TouchableOpacity
+        onPress={()=>{props.onPressing()}} style={styles.mainContainer}>
+                <Image style={styles.image} source={{uri: props.imgURL}} />
+                <View style={styles.textContainer}>
+                    <Text style={styles.title}>{props.title}</Text>
+                    <Text style={styles.description}>{props.desc}</Text>
+                    <Text style={styles.price}>{props.price}</Text>
+                </View>
+            
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        width:"100%",
+        height:250,
+        paddingHorizontal:10,
+    },
+    image: {
+       //position: 'absolute',
+        height: 170,
+        width: 170,
+        
+    },
+    textContainer: {
+        position: 'relative',
+        top: 0,
+        left: 10,
+        right: 120,
+        
+    },
+    title: {
+        color: '#ffffff',
+        opacity: 0.8,
+        fontSize: 20,
+        fontStyle: 'italic',
+    },
+    description: {
+        color: '#ffffff',
+        opacity: 0.6,
+    },
+    price: {
+        color: '#ffffff',
+        opacity: 0.6,
+        fontStyle: 'italic',
+    },
+})
